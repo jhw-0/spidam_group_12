cebc8c1ff952473422650dfa34e826eb6e465285
Jared Wallace <jared_wallace@outlook.com>
2024-11-29 13:45:21 -0500

    Fixed test_jw.py to include module qualifiers. Added functions to model.py

--- model.py
+++ model.py
@@ -4,31 +4,49 @@ import pathlib
 
 class Model:
     def __init__(self):
-        self.audio_file = None # pathlib.Path(audio_file).resolve
-        self.file_type = None # self.audio_file.suffix
-        self.converted = None
+        self.audio_file = None
+        self.original_file_type = None
+        self.new_audio_file = None
 
     def set_audio_file(self, audio_file : str):
         self.audio_file = audio_file
+
     def get_name(self):
         return self.audio_file.name
     
-    def check_wav(self, path : str) -> bool:
-        return os.path.splitext(path)[1] == '.wav'
+    def check_wav(self) -> bool:
+        return self.audio_file == '.wav'
     
     def audio_file_button(self) -> str:
         return input('What is the path of your audio file?')
     
-    # need to use pathlib.Path object, not string
-    def convert_to_wav(self, path : pathlib.Path) -> str:
-        supported_ext = '.mp3', '.flv', '.mp4', '.wma', '.ogg'
-        
-        dst = os.path.splitext(path)[0] + '.wav'
-        ext = os.path.splitext(path)[1]
-        if ext in supported_ext:
-            pydub.AudioSegment.from_file(path, ext[1:]).export(dst, format='wav')
+    def convert_to_wav(self) -> None:
+        # tuple representing supported extensions for file type conversion
+        supported_extensions = '.mp3', '.flv', '.mp4', '.wma', '.ogg'
+        self.original_file_type = self.audio_file.suffix
+        if self.original_file_type in supported_extensions:
+            pydub.AudioSegment.from_file(
+                self.audio_file,
+                extension
+            ).export(
+                destination := self.audio_file.with_suffix('.wav'),
+                format='wav'
+            )
+            self.new_audio_file = destination
         else:
             print('Sorry. File type not supported. Instead, try any of: ')
-            for ext in supported_ext:
-                print(ext)
-        return dst
+            for extension in supported_extensions:
+                print(extension)
+
+    def check_meta(self) -> bool:
+        pass
+
+    def check_two_channel(self) -> bool:
+        pass
+
+    def convert_to_one_channel(self) -> None:
+        pass
+
+    def remove_meta(self):
+        pass
+        
